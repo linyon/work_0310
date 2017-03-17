34f08460d339417e9dbf8380ab9e23be3ad7b077
linyon <asd357076@gmail.com>
2017-03-17 09:42:25 +0800

    20170317

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -8,38 +8,56 @@ using Newtonsoft.Json.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using WebApplication1.Models;
+using System.Runtime.Caching;
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
         public async Task<ActionResult> Index()
         {
-            string Target_Uri = "http://opendata.epa.gov.tw/ws/Data/ATM00323/?$skip=0&$top=1000&format=json";
+            /* string Target_Uri = "http://opendata.epa.gov.tw/ws/Data/ATM00323/?$skip=0&$top=1000&format=json";
+
+             HttpClient client = new HttpClient();
+             client.MaxResponseContentBufferSize = Int32.MaxValue;
+             var response = await client.GetStringAsync(Target_Uri);
+             var collection = JsonConvert.DeserializeObject<IEnumerable<info_json>>(response);
+             return View(collection);*/
+            var Source = await this.GetData();
+            ViewData.Model = Source;
+            return View();
+        }
+        private async Task<IEnumerable<info_json>> GetData()
+        {
+            string cache_name = "air_";
+            ObjectCache cache = MemoryCache.Default;
+            CacheItem cacheContents = cache.GetCacheItem(cache_name);
+
+            if (cacheContents == null)
+            {
+                return await RetriveData(cache_name);
+            }
+            else
+            {
+                return cacheContents.Value as IEnumerable<info_json>;
+            }
+        }
+        private async Task<IEnumerable<info_json>> RetriveData(string cache_name)
+        {
+            string targetURI = "http://opendata.epa.gov.tw/ws/Data/ATM00323/?$skip=0&$top=1000&format=json";
 
             HttpClient client = new HttpClient();
             client.MaxResponseContentBufferSize = Int32.MaxValue;
-            var response = await client.GetStringAsync(Target_Uri);
+            var response = await client.GetStringAsync(targetURI);
+
             var collection = JsonConvert.DeserializeObject<IEnumerable<info_json>>(response);
-            //ViewBag.Result = response;
-            return View(collection);
+
+            CacheItemPolicy policy = new CacheItemPolicy();
+            policy.AbsoluteExpiration = DateTime.Now.AddMinutes(30);
+
+            ObjectCache cacheItem = MemoryCache.Default;
+            cacheItem.Add(cache_name, collection, policy);
+
+            return collection;
         }
-        /*public ActionResult Index()
-        {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }*/
     }
 }
--- WebApplication1/WebApplication1/Models/info_json.cs
+++ WebApplication1/WebApplication1/Models/info_json.cs
@@ -7,29 +7,29 @@ namespace WebApplication1.Models
 {
     public class info_json
     {
-        [Display(Name = "測站代碼‵")]
+        [Display(Name = "測站代碼")]
         public string SiteId { get; set; }
 
-        [Display(Name = "測站名稱‵")]
+        [Display(Name = "測站名稱")]
         public string SiteName { get; set; }
 
-        [Display(Name = "測項代碼‵")]
+        [Display(Name = "測項代碼")]
         public string ItemId { get; set; }
 
-        [Display(Name = "測項名稱‵")]
+        [Display(Name = "測項名稱")]
         public string ItemName { get; set; }
 
-        [Display(Name = "測項英文名稱‵")]
+        [Display(Name = "測項英文名稱")]
         public string ItemEngName { get; set; }
 
-        [Display(Name = "測項單位‵")]
+        [Display(Name = "測項單位")]
 
         public string ItemUnit { get; set; }
 
-        [Display(Name = "監測日期‵")]
+        [Display(Name = "監測日期")]
         public string MonitorMonth { get; set; }
 
-        [Display(Name = "數值‵")]
+        [Display(Name = "數值")]
         public string Concentration { get; set; }
 
     }
